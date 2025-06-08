c3358eb6eb60de0f6d98f75048f06df10f361161
Vladn09 <nozhik09@gmail.com>
2025-06-08 16:07:16 +0200

    heals

--- src/main/java/com/example/Sprachraume/Role/repository/RoleRepository.java
+++ src/main/java/com/example/Sprachraume/Role/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.example.Sprachraume.Role.repository;
 
 import com.example.Sprachraume.Role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
     Role findByTitle (String title);
 }
--- src/main/java/com/example/Sprachraume/Rooms/repository/RoomRepository.java
+++ src/main/java/com/example/Sprachraume/Rooms/repository/RoomRepository.java
@@ -5,14 +5,18 @@ import com.example.Sprachraume.Rooms.entity.Room;
 import com.example.Sprachraume.UserData.entity.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface RoomRepository extends JpaRepository<Room, Long> , JpaSpecificationExecutor<Room> {
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
     Optional<Room> findById(Long id);
 
-    List<Room> findAllByCreator (UserData userData);
+    List<Room> findAllByCreator(UserData userData);
+
     List<Room> findRoomByParticipantsId(Long participantId);
+
     List<Room> findByStatusTrue();
 }
