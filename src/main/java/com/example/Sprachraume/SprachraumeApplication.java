874f6a037f5dbce42797a7e4e7cbb639b031d9ce
Vladn09 <nozhik09@gmail.com>
2026-04-20 12:06:48 +0200

    last commit

--- src/main/java/com/example/Sprachraume/SprachraumeApplication.java
+++ src/main/java/com/example/Sprachraume/SprachraumeApplication.java
@@ -1,23 +1,3 @@
-//package com.example.Sprachraume;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//
-//@EnableScheduling
-//@SpringBootApplication
-//public class SprachraumeApplication {
-//
-//	public static void main(String[] args) {
-//		System.out.println(">>>>> Starting Sprachraume...");
-//		SpringApplication.run(SprachraumeApplication.class, args);
-//		System.out.println(">>>>> УТВ Sprachraume...");
-//	}
-//
-//}
-//
-//
-
 package com.example.Sprachraume;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -30,15 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		servers = {
 				@Server(
 						url = "https://sprachraume-production.up.railway.app/api",
-						description = "UNIQUE_SWAGGER_TEST_999"
+						description = "Production server"
 				)
 		}
 )
 public class SprachraumeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("=== START UNIQUE_SWAGGER_TEST_999 ===");
 		SpringApplication.run(SprachraumeApplication.class, args);
 	}
 }
-
